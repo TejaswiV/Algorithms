6f6f6e0be039ee26f7cef7264a783131018ec469
Tejaswi Venupalli <tejaswi.venupalli@gmail.com>
2018-08-10 13:33:30 +1000

    Add files via upload

---
+++ src/main/com/teju/algorithms/linkedlist/LinkedList.java
@@ -0,0 +1,78 @@
+package com.teju.algorithms.linkedlist;
+
+public class LinkedList<T> {
+
+    Node<T> head;
+
+    public void append(T data) {
+
+        Node<T> n = new Node<T>(data);
+        if (head == null) {
+            head = n;
+            return;
+        }
+        Node<T> current = head;
+        while (current.next != null) {
+            current = current.next;
+        }
+        current.next = n;
+    }
+
+    public void prepend(T data) {
+        if (head == null) {
+            head = new Node<T>(data);
+            return;
+        }
+        Node<T> oldHead = head;
+        head = new Node<T>(data);
+        head.next = oldHead;
+
+    }
+
+    public void delete(T data) {
+        if (head.data == data) {
+            head = head.next;
+        }
+
+        Node<T> current = head;
+        while (current.next != null) {
+            if (current.next.data == data) {
+                current.next = current.next.next;
+            }
+            current = current.next;
+        }
+    }
+
+    public void printList() {
+        Node<T> current = head;
+        while (current != null) {
+            System.out.print(current.data + "  ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+
+    public int getSize(){
+        int size = 0;
+
+        Node<T> current = head;
+        while(current != null){
+            size ++;
+            current = current.next;
+        }
+
+        return size;
+    }
+
+}
+
+class Node<T> {
+    Node<T> next;
+    T data;
+
+    public Node(T data) {
+        this.data = data;
+        this.next = null;
+    }
+
+}
---
+++ src/main/com/teju/algorithms/linkedlist/RunnerTechnique.java
@@ -0,0 +1,48 @@
+package com.teju.algorithms.linkedlist;
+
+public class RunnerTechnique {
+
+/*
+    Rearrange a1->a2->a3->a4->a5->b1->b2->b3->b4->b5
+    to        a1->b1->a2->b2->a3->b3->a4->b4->a5->b5
+
+*/
+    public void rearrange(LinkedList<String> list){
+
+        Node<String> slow = list.head;
+        Node<String> fast = list.head;
+
+
+        /*
+		 * After this loop slow pointer will be in the middle and fast pointer will point to the head
+		 *
+		 *
+		 */
+        while(slow != null && fast != null && fast.next != null){
+
+            if(fast.next.next == null){ // because this type of list size is even always
+                fast = list.head;
+                break;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+
+        while(slow != null && fast != null && slow.next != null && fast.next != null){
+
+            Node<String> temp = slow.next;
+            slow.next = slow.next.next;
+
+            temp.next = fast.next;
+            fast.next = temp;
+
+            fast = fast.next.next;
+
+        }
+
+
+    }
+
+
+}
